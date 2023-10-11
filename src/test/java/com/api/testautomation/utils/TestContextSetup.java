a2c85de30f6edf12b89da673932aac680249e98a
jsnim <jsnimmi@yahoo.com>
2023-10-11 12:17:31 +0800

    Committed completion of API and UI changes in OpenHeimer project

--- src/test/java/com/api/testautomation/utils/TestContextSetup.java
+++ src/test/java/com/api/testautomation/utils/TestContextSetup.java
@@ -8,24 +8,15 @@ import java.io.IOException;
 
 public class TestContextSetup {
 
-	public WebDriver driver;
-	public String landingPageProductName;
-	public PageObjectManager pageObjectManager;
-	//public TestBase testBase;
-	//public GenericUtils genericUtils;
-	//private WebDriver driver = Hooks.driver;
-//BaseClass baseClass=new BaseClass(driver);
+    public WebDriver driver;
+    public String landingPageProductName;
+    public PageObjectManager pageObjectManager;
 
-	
-	public TestContextSetup() throws IOException
-	{
-		/*testBase = new TestBase();
+    public TestContextSetup() throws IOException {
+
+      /*testBase = new TestBase();
 		pageObjectManager = new PageObjectManager(testBase.WebDriverManager());
 		genericUtils = new GenericUtils(testBase.WebDriverManager());
-		
 		*/
-		
-
-	}
-	
+    }
 }
